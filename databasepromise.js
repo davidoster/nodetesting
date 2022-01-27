7e658c3befae3d11cf48f0fd7afb47572f2cb875
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-01-27 22:03:31 +0200

    Just about to run ... Build the Promise code

--- databasepromise.js
+++ databasepromise.js
@@ -14,39 +14,73 @@ connectionConnect():
 */
 var mysql = require("mysql2");
 class DatabasePromise {
+
     constructor(configDetails, query) {
         this.configDetails = configDetails;
         this.query = query;
         this.resolve = null;
         this.reject = null;
-        this.promise = new Promise(resolve, reject);
-        initializeConnection();
-    }
-
-    initializeConnection() {
-        this.connection = mysql.createConnection(configDetails);
-        this.connection.connect(this.connectionConnect(error, result, fields));
+        this._connection = null;
+        // this.promise = new Promise(this.resolve, this.reject);
+        // this.initializeConnection.bind(this);
+        // inside Promise
+            this.initializeConnection();
+        // end of Promise
         
     }
 
-    connectionConnect(error, result, fields) {
-        if(error) {
-            reject(`Error connecting: ${error}`);
-        }
-        else {
-            let finalRows = [];
-            console.log("Connected!");
-            
-            this.connection.query(sql2, (err, rows, fields) => {
-                if (err) {
-                    reject("Error in sql query");
-                }
-                finalRows = rows;
-                resolve(finalRows);
-            });
-            this.connection.end();
-        }
+    get connection(){
+        return(this._connection);
     }
+
+    set connection(connection) {
+        this._connection = connection;
+        console.log(this._connection);
+    }
+
+    initializeConnection() {
+        this.connection = mysql.createConnection(this.configDetails);
+        // console.log(this._connection);
+        this.connection.connect(function(error, result, fields) {
+            
+            if(error) {
+                console.log(`Error connecting: ${error}`);
+            }
+            else {
+                let finalRows = [];
+                console.log("Connected!");
+                
+                this.connection.query(this.query, (err, rows, fields) => {
+                    if (err) {
+                        console.log("Error in sql query");
+                    }
+                    finalRows = rows;
+                    console.log(finalRows);
+                });
+                this.connection.end();
+            }
+        });
+        
+    }
+
+    // connectionConnect(error, result, fields) {
+    //     if(error) {
+    //         console.log(`Error connecting: ${error}`);
+    //     }
+    //     else {
+    //         let finalRows = [];
+    //         console.log("Connected!");
+            
+    //         this.connection.query(this.query, (err, rows, fields) => {
+    //             if (err) {
+    //                 console.log("Error in sql query");
+    //             }
+    //             finalRows = rows;
+    //             console.log(finalRows);
+    //         });
+    //         this.connection.end();
+    //     }
+    // }
 }
 
 module.exports = { DatabasePromise }
@@ -55,11 +89,11 @@ module.exports = { DatabasePromise }
 // this goes to another file that imports require()
 let remote_dbhost = {
     host: "ra1.anystream.eu", // 127.0.0.1 or localhost
-    port: "54200",
+    port: "5420",
     user: "cb12ptjs",
     password: "cb12ptjs",
     database: "cb12ptjs"
 };
 let query = "SELECT * FROM `cb12ptjs`.`customer`;";
 let myDatabasePromise = new DatabasePromise(remote_dbhost, query);
-console.log(myDatabasePromise.results());
+// console.log(myDatabasePromise.results());
